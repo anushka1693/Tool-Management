9fba79bf2e1dd1260cc138d7410019c991498680
anushka1693 <anushka.shetty@knavus.com>
2026-04-16 16:10:43 +0530

    Update index.js

--- api/getTools/index.js
+++ api/getTools/index.js
@@ -1,1 +1,34 @@
+const { TableClient } = require("@azure/data-tables");
 
+module.exports = async function (context, req) {
+
+  try {
+
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+
+    const tableClient = TableClient.fromConnectionString(
+      connectionString,
+      "ToolRequests"
+    );
+
+    const entities = [];
+
+    for await (const entity of tableClient.listEntities()) {
+      entities.push(entity);
+    }
+
+    context.res = {
+      status: 200,
+      body: entities
+    };
+
+  } catch (error) {
+
+    context.res = {
+      status: 500,
+      body: { error: error.message }
+    };
+
+  }
+
+};
