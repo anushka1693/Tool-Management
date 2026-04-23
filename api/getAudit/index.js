346fbd51d3ada945e02908ff5bada75099362b7c
anushka1693 <anushka.shetty@knavus.com>
2026-04-23 18:45:08 +0530

    Create index.js

---
+++ api/getAudit/index.js
@@ -0,0 +1,37 @@
+const { TableClient } = require("@azure/data-tables");
+
+module.exports = async function (context, req) {
+
+  try {
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+
+    const tableClient = TableClient.fromConnectionString(
+      connectionString,
+      "AuditLogs"
+    );
+
+    const toolId = req.query.toolId;
+
+    const entities = [];
+
+    for await (const entity of tableClient.listEntities({
+      queryOptions: {
+        filter: `PartitionKey eq '${toolId}'`
+      }
+    })) {
+      entities.push(entity);
+    }
+
+    context.res = {
+      status: 200,
+      body: entities
+    };
+
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message
+    };
+  }
+
+};
